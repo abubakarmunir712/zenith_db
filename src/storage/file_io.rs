6c8d4223a157793abfb3ee6621815a947d61ef59
Abubakar <abubakarmunir712@gmail.com>
2025-03-23 03:11:36 +0500

    Format file_io file

--- src/storage/file_io.rs
+++ src/storage/file_io.rs
@@ -1,30 +1,27 @@
 // file_io.rs - Handles low-level file I/O operations for the storage engine.
 //
-// This file contains essential functions for reading and writing pages 
-// to disk, ensuring efficient and reliable data persistence. It provides 
-// abstractions over system file operations, including handling page-aligned 
+// This file contains essential functions for reading and writing pages
+// to disk, ensuring efficient and reliable data persistence. It provides
+// abstractions over system file operations, including handling page-aligned
 // reads/writes, flushing data, and managing file descriptors.
 //
-// File I/O is a critical component of the storage layer, interacting 
+// File I/O is a critical component of the storage layer, interacting
 // closely with the buffer pool and WAL (Write-Ahead Logging) system.
 //
-use std::fs::{self, OpenOptions,File};
-use std::io::{Result, Error, ErrorKind,Write,Seek,SeekFrom,Read};
+use std::fs::{self, File, OpenOptions};
+use std::io::{Error, ErrorKind, Read, Result, Seek, SeekFrom, Write};
 pub struct IOEngine;
 
-impl IOEngine{
-
-
+impl IOEngine {
     pub fn file_exists(filename: &str) -> bool {
         fs::metadata(filename).is_ok()
     }
-    
 
     pub fn create_file(filename: &str) -> Result<bool> {
         if Self::file_exists(filename) {
             return Ok(false); // File already exists
         }
-    
+
         OpenOptions::new()
             .write(true)
             .create(true)
@@ -33,10 +30,13 @@ impl IOEngine{
             .map_err(|e| Error::new(ErrorKind::Other, format!("Failed to create file: {}", e))) // it propagate errors
     }
 
-      /// this function appends a page of 4kb (4096 bytes) in our file.
-      pub fn write_page(filename: &str, data: &[u8]) -> Result<()> {
+    /// this function appends a page of 4kb (4096 bytes) in our file.
+    pub fn write_page(filename: &str, data: &[u8]) -> Result<()> {
         if data.len() != 4096 {
-            return Err(Error::new(ErrorKind::InvalidInput, "Data must be exactly be equal to a page i.e 4096 bytes"));
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "Data must be exactly be equal to a page i.e 4096 bytes",
+            ));
         }
 
         let mut file = OpenOptions::new()
@@ -53,15 +53,14 @@ impl IOEngine{
     //This function reads a specific page from a given file and the page number.
     pub fn read_page(filename: &str, page_number: u64) -> Result<[u8; 4096]> {
         let mut file = File::open(filename)?;
-    
-        let offset = page_number * 4096;//it calculates the offset considering it 0 index
+
+        let offset = page_number * 4096; //it calculates the offset considering it 0 index
         file.seek(SeekFrom::Start(offset))?; // moves to the correct page
-    
+
         //making a fixed size array of 4kb and reading exact a page into it.
-        let mut buffer = [0; 4096]; 
-        file.read_exact(&mut buffer)?; 
-    
+        let mut buffer = [0; 4096];
+        file.read_exact(&mut buffer)?;
+
         Ok(buffer)
     }
-    
 }
